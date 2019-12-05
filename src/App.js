80f7f59b772ad52f81b1e9f7300381226bd46053
Graham G <ggilpatric@vgsi.com>
2019-12-05 18:20:41 -0500

    drawer opens

--- src/App.js
+++ src/App.js
@@ -4,37 +4,46 @@ import sha256 from 'js-sha256';
 const qz = require('qz-tray');
 
 const App = () => {
-  qz.api.setSha256Type(function(data) { return sha256(data); });
+  qz.api.setSha256Type(data => sha256(data));
   qz.api.setPromiseType(function promise(resolver) { return new Promise(resolver); });
 
   useEffect(() => {
     qz.websocket.connect()
-    .then(qz.printers.find)
-    .then((printers) => {
-       console.log(printers);
-    })
+    // const findPrinters = () => {
+    //   qz.printers.find().then(data => console.log(data))
+    // }
   }, [])
 
-  // const findPrinters = () => {
-  //   qz.printers.find().then(data => console.log(data))
-  // }
-
-  const printAndOpen = () => {
+  const openDrawer = () => {
     const config = qz.configs.create("EPSON TM-U675 Receipt")
     const data = [
-      { type: 'raw', data: '', options: { language: 'ESCPOS', dotDensity: 'single' } },
-      '\x1B' + '\x40', 
-      'Print me pls \n\n\n\n\n\n\n\n',
-      '\x1B' + '\x69',
-      '\x1B' + '\x70' + '\x00' + '\x40' + '\xF0'
+      { type: 'raw', format: 'hex', data: 'x1Bx70x01xC8xFA' }
   ];
-   qz.print(config, data).catch(function(e) { console.error(e); });
+   qz.print(config, data).catch((e) => console.error(e));
+  }
+
+  const printSomething = () => {
+    const config = qz.configs.create("EPSON TM-U675 Receipt")
+    const data = [
+      { type: 'raw', data: 'Print me pls \n\n\n\n\n\n\n\n', options: { language: 'ESCPOS', dotDensity: 'single' }},
+  ];
+   qz.print(config, data).catch((e) => console.error(e));
+  }
+
+  const validateSlip = () => {
+    const config = qz.configs.create("EPSON TM-U675 Slip")
+    const data = [
+      { type: 'raw', format: 'hex', data: 'x1Bx57'},
+  ];
+   qz.print(config, data).catch((e) => console.error(e));
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => {printAndOpen()}}>~*Click Here*~</button>
+        <button onClick={() => {openDrawer()}}>Open</button>
+        <button onClick={() => {printSomething()}}>Print</button>
+        <button onClick={() => {validateSlip()}}>Validate</button>
       </header>
     </div>
   );
